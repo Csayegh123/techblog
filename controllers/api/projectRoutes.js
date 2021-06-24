e1fcc01c4e42755defc5f16508b43d01084311f5
Christopher <sayegh.topher@gmail.com>
2021-06-24 18:40:01 -0500

    added api

--- controllers/api/projectRoutes.js
+++ controllers/api/projectRoutes.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const { Project } = require('../../models');
+const withAuth = require('../../utils/auth');
 
-
-router.post('/',  async (req, res) => {
+router.post('/', withAuth, async (req, res) => {
   try {
     const newProject = await Project.create({
       ...req.body,
@@ -15,7 +15,7 @@ router.post('/',  async (req, res) => {
   }
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', withAuth, async (req, res) => {
   try {
     const projectData = await Project.destroy({
       where: {
