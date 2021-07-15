63cc212b39decefc7182ba88f2e659a61da1a5fa
Christopher <sayegh.topher@gmail.com>
2021-07-15 18:49:32 -0500

    updated scritps pt2

--- controllers/index.js
+++ controllers/index.js
@@ -1,15 +1,8 @@
 const router = require('express').Router();
-
 const apiRoutes = require('./api');
-const homeRoutes = require('./home-routes');
-const dashboardRoutes = require('./dashboardRoutes');
+const homeRoutes = require('./homeRoutes');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
-router.use('/dashboard', dashboardRoutes);
-
-router.use((req, res) => {
-    res.status(404).end();
-})
 
 module.exports = router;
